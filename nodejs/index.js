751113f1898c672bd1803633bc60b7b1416d840e
FUJI Goro (gfx) <gfuji@cpan.org>
2017-10-22 14:27:17 +0900

    expand N+1 queries in GET /message

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const fs = require('fs')
 const path = require('path')
 const crypto = require('crypto')
@@ -221,7 +223,7 @@ function formatDate (dateStr) {
 }
 
 app.get('/message', getMessage)
-function getMessage(req, res) {
+async function getMessage(req, res) {
   const { userId } = req.session
   if (!userId) {
     res.status(403).end()
@@ -229,34 +231,42 @@ function getMessage(req, res) {
   }
 
   const { channel_id, last_message_id } = req.query
-  return pool.query('SELECT * FROM message WHERE id > ? AND channel_id = ? ORDER BY id DESC LIMIT 100', [last_message_id, channel_id])
-    .then(rows => {
-      const response = []
-      let p = Promise.resolve()
-      rows.forEach((row, i) => {
-        const r = {}
-        r.id = row.id
-        p = p.then(() => {
-          return pool.query('SELECT name, display_name, avatar_icon FROM user WHERE id = ?', [row.user_id])
-            .then(([user]) => {
-              r.user = user
-              r.date = formatDate(row.created_at)
-              r.content = row.content
-              response[i] = r
-            })
-        })
-      })
 
-      return p.then(() => {
-        response.reverse()
-        const maxMessageId = rows.length ? Math.max(...rows.map(r => r.id)) : 0
-        return pool.query(`INSERT INTO haveread (user_id, channel_id, message_id, updated_at, created_at)
-          VALUES (?, ?, ?, NOW(), NOW())
-          ON DUPLICATE KEY UPDATE message_id = ?, updated_at = NOW()`,
-          [userId, channel_id, maxMessageId, maxMessageId])
-          .then(() => res.json(response))
-      })
-    })
+  const rows = await pool.query(
+    `
+      select
+        message.*
+        , user.name as user_name
+        , user.display_name as user_display_name
+        , user.avatar_icon as user_avatar_icon
+      from message
+      join user on message.user_id = user.id
+      where id > ? and channel_id = ? order by id desc limit 100
+    `, [last_message_id, channel_id]);
+
+  const response = rows.map(row => {
+    return {
+      id: row.id,
+      date: formatDate(row.created_at),
+      content: row.content,
+
+      user: {
+        name: row.user_name,
+        display_name: row.user_display_name,
+        avatar_icon: row.user_avatar_icon,
+      },
+    };
+  });
+  response.reverse();
+
+  const maxMessageId = rows.length ? Math.max(...rows.map(r => r.id)) : 0;
+
+  await pool.query(`INSERT INTO haveread (user_id, channel_id, message_id, updated_at, created_at)
+  VALUES (?, ?, ?, NOW(), NOW())
+  ON DUPLICATE KEY UPDATE message_id = ?, updated_at = NOW()`,
+  [userId, channel_id, maxMessageId, maxMessageId]);
+
+  res.json(response); // TODO: insert into haveread の前でもいいかも
 }
 
 function sleep (seconds) {
