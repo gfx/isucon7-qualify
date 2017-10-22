c298644bd30a80f2cd02cfe363c92a84052d840c
FUJI Goro (gfx) <gfuji@cpan.org>
2017-10-22 17:42:07 +0900

    sleep(0.1)

--- nodejs/index.js
+++ nodejs/index.js
@@ -286,7 +286,7 @@ function fetchUnread(req, res) {
     return
   }
 
-  return sleep(0.5)
+  return sleep(0.1)
     .then(() => pool.query('select channel_id as id, count(*) as count from message group by channel_id'))
     .then(channels => {
       return Promise.all([
